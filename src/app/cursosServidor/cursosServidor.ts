241d90c603d9ea50049731a47186c7de3fff0ef4
Sebastian Pelaez <pelaez.seba@gmail.com>
2024-06-22 15:38:47 -0300

    checkeo b64

--- src/app/cursosServidor/cursosServidor.ts
+++ src/app/cursosServidor/cursosServidor.ts
@@ -74,7 +74,7 @@ export class cursosServidorComponent {
       }
     } catch (error) {
       console.error('Error al realizar la solicitud:', error);
-      alert('Error en la búsqueda. Intente luego o consulte al administrador del sistema.');
+      alert('Error al descargar el curso. Intente luego o consulte al administrador del sistema.');
       this.showAlert = true;
     }
   }
@@ -90,21 +90,26 @@ export class cursosServidorComponent {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json; charset=utf-8',
-
           'Authorization': `Bearer ${this.token}`,
         }
       });
 
       if (response.ok) {
         const cursoB64 = await response.json();
-        // Convertir el base64 de la salida en JSON
-        const binaryString = atob(cursoB64);
-        // Convertir la cadena binaria a una cadena UTF-8
-        const utf8String = decodeURIComponent(escape(binaryString));
-        const cursoJson = JSON.parse(utf8String);
-        console.log(cursoJson);
-        this.descargarCurso(cursoJson);
 
+        if (this.isBase64(cursoB64)) {
+          // Convertir el base64 de la salida en JSON
+          const binaryString = atob(cursoB64);
+          // Convertir la cadena binaria a una cadena UTF-8
+          const utf8String = decodeURIComponent(escape(binaryString));
+          const cursoJson = JSON.parse(utf8String);
+          console.log(cursoJson);
+          this.descargarCurso(cursoJson);
+        } else {
+          console.error('La respuesta no es una cadena base64 válida');
+          alert('Error al descargar el curso. Intente luego o consulte al administrador del sistema.');
+          this.showAlert = true;
+        }
       } else {
         console.log('Ha ocurrido un error:', response.status);
         alert('Error en la búsqueda. Intente luego o consulte al administrador del sistema.');
@@ -115,7 +120,6 @@ export class cursosServidorComponent {
       alert('Error en la búsqueda. Intente luego o consulte al administrador del sistema.');
       this.showAlert = true;
     }
-
   }
 
   async getNombre(urlServidor: string) {
@@ -205,7 +209,7 @@ export class cursosServidorComponent {
           this.goHome();
         } else {
           console.log('Ha ocurrido un error, ', response.status);
-          alert('Error al modificar el curso. Intente nuevamente.');
+          alert('Error al descargar el curso. Intente nuevamente.');
           this.showAlert = true;
         }
       } catch (e) {
@@ -221,5 +225,11 @@ export class cursosServidorComponent {
     }
   }
 
-
+  private isBase64(str: string): boolean {
+    try {
+      return btoa(atob(str)) === str;
+    } catch (err) {
+      return false;
+    }
+  }
 }
